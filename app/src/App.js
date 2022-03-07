4b5b43377bbac1724f3469435dcb5c72f06b4ed4
Jashu <jb779@njit.edu>
2022-03-07 12:48:48 -0500

    added button functionality

--- app/src/App.js
+++ app/src/App.js
@@ -64,7 +64,7 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🍭 SreeNub Nubest Drop</p>
+          <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
           {!walletAddress && renderNotConnectedContainer()}
         </div>
